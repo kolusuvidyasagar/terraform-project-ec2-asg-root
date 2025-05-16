da252674dab4ad3d61154ba81da27921451744e6
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 22:14:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "asg_name" {
 output "launch_template_id" {
   value = aws_launch_template.this.id
 }
+
+output "security_group_id" {
+  value = aws_security_group.asg_sg.id
+  description = "ID of the security group created"
+}
