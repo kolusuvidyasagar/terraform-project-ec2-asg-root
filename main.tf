484b1a4289da7f3ac4a95bc72852412bdaaf4278
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 22:08:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "aws_launch_template" "this" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name = var.key_name
- vpc_security_group_ids = var.security_group_ids
-  #vpc_security_group_ids = [aws_security_group.asg_sg.id]
+ #vpc_security_group_ids = var.security_group_ids
+  vpc_security_group_ids = [aws_security_group.asg_sg.id]
 
   tag_specifications {
     resource_type = "instance"
