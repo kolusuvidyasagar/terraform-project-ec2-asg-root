9237649d18a85f7a15d14378df8008f87fcef069
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 12:45:47 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+ami_id             = "ami-084568db4383264d4"
+subnet_ids         = ["subnet-0a3ed19a2396b5694", "subnet-0b331a653df5d143c"]
+security_group_ids = ["sg-0123456789abcdef0"]
+vpc_id     = "vpc-0d506225391c9e05b"
+key_name        = "eks"  
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project or environment name"
+  default     = "demo"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "min_size" {
+  default = 1
+}
+
+variable "max_size" {
+  default = 3
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  description = "List of subnet IDs for the ASG"
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  description = "Security group IDs"
+}
+variable "vpc_id" {
+  description = "VPC ID for the Security Group"
+  type        = string
+}
+variable "key_name" {
+  description = "Name of the EC2 Key Pair"
+  type        = string
+}
